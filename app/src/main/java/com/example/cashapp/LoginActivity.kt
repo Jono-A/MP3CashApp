55e045f03feddfe9685018ed672ee5faf5bf84b6
Sean Amponin <amponinseangabriel@gmail.com>
2023-10-20 16:07:38 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/layout/activity_welcome.xml

--- app/src/main/java/com/example/cashapp/LoginActivity.kt
+++ app/src/main/java/com/example/cashapp/LoginActivity.kt
@@ -1,82 +1,81 @@
 package com.example.cashapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
-import com.example.cashapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityLoginBinding
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_login)
 
-        binding.btnLogin.setOnClickListener {
-            var errorCount = 0
+        emailEditText = findViewById(R.id.editTextTextEmailAddress)
+        passwordEditText = findViewById(R.id.editTextTextPassword)
+        loginButton = findViewById(R.id.loginbtn)
 
-            if (binding.email.text.isNullOrBlank())
-            {
-                errorCount++
-                binding.email.error = "This field is required"
-
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+            if (isValidEmail(email) && isValidPassword(password)) {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                startActivity(intent)
+            } else {
+                if (!isValidEmail(email)) {
+                    Toast.makeText(
+                        this,
+                        "Invalid email format. Please use your iacademy.edu.ph email.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this,
+                        "Invalid password. Password should be at least 8 characters long " +
+                                "and contain at least 1 number and 1 uppercase letter.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
             }
 
-            if (binding.password.text.isNullOrBlank())
-            {
-                errorCount++
-                binding.password.error = "This field is required"
-
-            }
-
-            if (binding.password.text.length < 8)
-            {
-                errorCount++
-                binding.password.error = "Password must contain at least 8 characters."
-
-            }
-
-            if (checkString(binding.password.text.toString()) == false)
-            {
-                errorCount++
-                binding.password.error = "Password must contain at least 1 number and uppercase letter."
-            }
-
-            if(errorCount <= 0){
+            //connect to welcome activity
+            if(isValidEmail(email) && isValidPassword(password)){
                 if (validateLogin(binding.email.text.toString(), binding.password.text.toString())){
                     val intent = Intent(this@LoginActivity,WelcomeActivity::class.java)
-                    intent.putExtra("email", binding.email.text.toString())
+                    intent.putExtra("email", binding.emaiL.text.toString())
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                     startActivity(intent)
-                    //finish()
 
                 } else {
                     Toast.makeText(this, "Invalid login credentials", Toast.LENGTH_SHORT).show()
                 }
             }
         }
+
     }
 
-    private fun validateLogin (email : String, password : String) : Boolean {
+    //validate functions for welcome activity
+    private fun validateLogin(email : String, password : String) : Boolean {
+        //send email and password to API and let API validate the credentials
+        //check API response
         return true
     }
 
-    private fun checkString(str: String): Boolean {
-        var ch: Char
-        var upperCaseFlag = false
-        var numberFlag = false
-        for (i in 0 until str.length) {
-            ch = str[i]
-            if (Character.isDigit(ch)) {
-                numberFlag = true
-            } else if (Character.isUpperCase(ch)) {
-                upperCaseFlag = true
-            }
+    private fun isValidPassword(password: String): Boolean {
+        val passwordPattern = "^(?=.*[0-9])(?=.*[A-Z]).{8,}$".toRegex()
+        return password.matches(passwordPattern)
+    }
 
-            if (numberFlag && upperCaseFlag) return true
-        }
-        return false
+    private fun isValidEmail(email: String): Boolean {
+        val emailPattern = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}".toRegex()
+        return email.matches(emailPattern)
     }
 }
--- app/src/main/java/com/example/cashapp/WelcomeActivity.kt
+++ app/src/main/java/com/example/cashapp/WelcomeActivity.kt
@@ -1,11 +1,5 @@
 package com.example.cashapp
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+class WelcomeActivity {
 
-class WelcomeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
-    }
 }
