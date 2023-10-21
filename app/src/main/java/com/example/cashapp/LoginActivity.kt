d69a714bcf69153bed50fb0fd56af4b860aefdc6
Jono Alcantara <202101054@iacademy.edu.ph>
2023-10-21 09:46:12 +0800

    jono updates

--- app/src/main/java/com/example/cashapp/LoginActivity.kt
+++ app/src/main/java/com/example/cashapp/LoginActivity.kt
@@ -13,40 +13,40 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnLogin.setOnClickListener {
+        binding.btnSignin.setOnClickListener {
             var errorCount = 0
 
-            if (binding.email.text.isNullOrBlank())
+            if (binding.tieEmail.text.isNullOrBlank())
             {
                 errorCount++
-                binding.email.error = "This field is required"
+                binding.tieEmail.error = "This field is required"
 
             }
 
-            if (binding.password.text.isNullOrBlank())
+            if (binding.tiePassword.text.isNullOrBlank())
             {
                 errorCount++
-                binding.password.error = "This field is required"
+                binding.tiePassword.error = "This field is required"
 
             }
 
-            if (binding.password.text.length < 8)
+            if (binding.tiePassword.text!!.length < 8)
             {
                 errorCount++
-                binding.password.error = "Password must contain at least 8 characters."
+                binding.tiePassword.error = "Password must contain at least 8 characters."
 
             }
 
-            if (checkString(binding.password.text.toString()) == false)
+            if (checkString(binding.tiePassword.text.toString()) == false)
             {
                 errorCount++
-                binding.password.error = "Password must contain at least 1 number and uppercase letter."
+                binding.tiePassword.error = "Password must contain at least 1 number and uppercase letter."
             }
 
             if(errorCount <= 0){
-                if (validateLogin(binding.email.text.toString(), binding.password.text.toString())){
+                if (validateLogin(binding.tieEmail.text.toString(), binding.tiePassword.text.toString())){
                     val intent = Intent(this@LoginActivity,WelcomeActivity::class.java)
-                    intent.putExtra("email", binding.email.text.toString())
+                    intent.putExtra("email", binding.tieEmail.text.toString())
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                     startActivity(intent)
